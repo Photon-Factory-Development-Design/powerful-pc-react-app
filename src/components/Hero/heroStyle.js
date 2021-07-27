0300906fb48c32ca85b5b31a95f2181385ba6f4b
TalentWebDev <49565243+talentwebdev@users.noreply.github.com>
2021-07-27 16:34:50 +0200

    primary completed.

--- src/components/Hero/heroStyle.js
+++ src/components/Hero/heroStyle.js
@@ -1,10 +1,11 @@
 const styles = () => ({
     root: {
         width: '100%',
-        height: '80vh',
+        height: '695.5px',
         '& video': {
             objectFit: 'cover'
-        }
+        },
+        marginTop: '-100px'
     }
 });
 
